01d7498791843e7f3d50c1c84da085db5492520c
angie <angheloalf95@gmail.com>
2023-03-11 11:18:41 -0300

    more header work

---
+++ include/libc/math.h
@@ -0,0 +1,13 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include "libultra.h"
+
+f64 __fint(f64 x);
+f64 fmod(f64 x, f64 y);
+f64 ceil(f64 x);
+f64 floor(f64 x);
+
+f64 modf(f64 x, f64 *ipart);
+
+#endif
---
+++ include/unknown_structs.h
@@ -0,0 +1,11 @@
+#ifndef UNKNOWN_STRUCTS_H
+#define UNKNOWN_STRUCTS_H
+
+#include "version.h"
+#include "libultra.h"
+#include "libc/stdint.h"
+#include "libc/stdbool.h"
+#include "unk.h"
+
+
+#endif
