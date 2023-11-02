0abe33b615deade11cee5cdcdf254766dde51283
Angie <angheloalf95@gmail.com>
2023-11-02 14:52:13 -0300

    update mapfile_parser to 2.X

--- tools/progress.py
+++ tools/progress.py
@@ -18,36 +18,41 @@ def getProgressFromMapFile(mapFile: mapfile_parser.MapFile, asmPath: Path, nonma
     totalStats = mapfile_parser.ProgressStats()
     progressPerFolder: dict[str, mapfile_parser.ProgressStats] = dict()
 
-    for file in mapFile:
-        if len(file.symbols) == 0:
-            continue
-
-        folder = file.filepath.parts[pathIndex]
-        if folder in aliases:
-            folder = aliases[folder]
-
-        if folder not in progressPerFolder:
-            progressPerFolder[folder] = mapfile_parser.ProgressStats()
-
-        originalFilePath = Path(*file.filepath.parts[pathIndex:])
-        fullAsmFile = asmPath / originalFilePath.with_suffix(".s")
-        wholeFileIsUndecomped = fullAsmFile.exists()
-
-        for func in file.symbols:
-            if func.name.endswith(".NON_MATCHING"):
+    for segment in mapFile:
+        for file in segment:
+            if len(file) == 0:
                 continue
 
-            funcNonMatching = f"{func.name}.NON_MATCHING"
+            folder = file.filepath.parts[pathIndex]
+            if folder in aliases:
+                folder = aliases[folder]
 
-            if wholeFileIsUndecomped:
-                totalStats.undecompedSize += func.size
-                progressPerFolder[folder].undecompedSize += func.size
-            elif mapFile.findSymbolByName(funcNonMatching) is not None:
-                totalStats.undecompedSize += func.size
-                progressPerFolder[folder].undecompedSize += func.size
-            else:
-                totalStats.decompedSize += func.size
-                progressPerFolder[folder].decompedSize += func.size
+            if folder not in progressPerFolder:
+                progressPerFolder[folder] = mapfile_parser.ProgressStats()
+
+            originalFilePath = Path(*file.filepath.parts[pathIndex:])
+            fullAsmFile = asmPath / originalFilePath.with_suffix(".s")
+            wholeFileIsUndecomped = fullAsmFile.exists()
+
+            for func in file:
+                if func.name.endswith(".NON_MATCHING"):
+                    continue
+
+                funcNonMatching = f"{func.name}.NON_MATCHING"
+
+                funcSize = 0
+                if func.size is not None:
+                    funcSize = func.size
+
+                if wholeFileIsUndecomped:
+                    totalStats.undecompedSize += funcSize
+                    progressPerFolder[folder].undecompedSize += funcSize
+                elif mapFile.findSymbolByName(funcNonMatching) is not None:
+                    totalStats.undecompedSize += funcSize
+                    progressPerFolder[folder].undecompedSize += funcSize
+                else:
+                    totalStats.decompedSize += funcSize
+                    progressPerFolder[folder].decompedSize += funcSize
 
     return totalStats, progressPerFolder
 
@@ -56,30 +61,33 @@ def getProgress(mapPath: Path, version: str) -> tuple[mapfile_parser.ProgressSta
     mapFile = mapfile_parser.MapFile()
     mapFile.readMapFile(mapPath)
 
-    for file in mapFile:
-        if len(file.symbols) == 0:
-            continue
+    for segment in mapFile:
+        for file in segment:
+            if len(file) == 0:
+                continue
 
-        filepathParts = list(file.filepath.parts)
-        if version in filepathParts:
-            filepathParts.remove(version)
-        file.filepath = Path(*filepathParts)
+            filepathParts = list(file.filepath.parts)
+            if version in filepathParts:
+                filepathParts.remove(version)
+            file.filepath = Path(*filepathParts)
 
-        # Fix symbol size calculation because of NON_MATCHING symbols
-        for sym in file.symbols:
-            if sym.name.endswith(".NON_MATCHING") and sym.size != 0:
-                realSym = file.findSymbolByName(sym.name.replace(".NON_MATCHING", ""))
-                if realSym is not None and realSym.size == 0:
-                    realSym.size = sym.size
-                    sym.size = 0
+            # Fix symbol size calculation because of NON_MATCHING symbols
+            for sym in file:
+                if sym.name.endswith(".NON_MATCHING") and sym.size != 0:
+                    realSym = file.findSymbolByName(sym.name.replace(".NON_MATCHING", ""))
+                    if realSym is not None and realSym.size == 0:
+                        realSym.size = sym.size
+                        sym.size = 0
 
     nonMatchingsPath = ASMPATH / version / NONMATCHINGS
 
-    return getProgressFromMapFile(mapFile.filterBySegmentType(".text"), ASMPATH / version, nonMatchingsPath, aliases={"ultralib": "libultra"})
+    return getProgressFromMapFile(mapFile.filterBySectionType(".text"), ASMPATH / version, nonMatchingsPath, aliases={"ultralib": "libultra"})
 
 def progressMain():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", help="version to process", default="usa")
+    parser.add_argument("-p", "--subpaths", help="Make a summary for one level deeper in the path tree", action="store_true")
+    parser.add_argument("-s", "--sort", help="Sort by decomped size instead of the ROM sorting", action="store_true")
 
     args = parser.parse_args()
 
@@ -87,7 +95,15 @@ def progressMain():
 
     totalStats, progressPerFolder = getProgress(mapPath, args.version)
 
-    mapfile_parser.progress_stats.printStats(totalStats, progressPerFolder)
+    mapfile_parser.ProgressStats.printHeader()
+    totalStats.print("all", totalStats)
+    print()
+
+    progressesList = list(progressPerFolder.items())
+    if args.sort:
+        progressesList.sort(key=lambda x: (x[1].decompedSize / x[1].total, x[1].total), reverse=True)
+    for folder, statsEntry in progressesList:
+        statsEntry.print(folder, totalStats)
 
 if __name__ == "__main__":
     progressMain()
