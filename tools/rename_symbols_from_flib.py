47457ae0761a3b302f77dc77b35bacfdc4958875
angie <angheloalf95@gmail.com>
2024-06-30 22:13:06 -0400

    eur & fra & ger: identify libultra data, rodata and bss

--- tools/rename_symbols_from_flib.py
+++ tools/rename_symbols_from_flib.py
@@ -1,21 +1,62 @@
-syms = {
+import argparse
+from pathlib import Path
+
+syms: dict[int, str] = {
 
 }
-found = set()
+found: set[int] = set()
+duped_addrs: set[int] = set()
+duped_names: set[str] = set()
+duped_syms: set[tuple[int, str]] = set()
 
 
-a = open("asdf.txt")
-for line in a:
-    if ", " not in line:
-        continue
-    name, addr, *_ = line.strip().split(", ")
-    addr = int(addr, 16)
-    syms[addr] = name
-a.close()
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath")
 
-FILEPATH = "linker_scripts/ger/symbol_addrs_main.txt"
+args = parser.parse_args()
 
-b = open(FILEPATH)
+FILEPATH = Path(args.filepath)
+
+def parse_flib_symbols(path: Path):
+    a = path.open()
+    for line in a:
+        if ", " not in line:
+            continue
+        name, addr, *_ = line.strip().split(", ")
+        addr = int(addr, 16)
+
+        if addr >= 0x90000000:
+            continue
+
+        if addr in syms:
+            duped_addrs.add(addr)
+            duped_syms.add((addr, name))
+            del syms[addr]
+            continue
+        if name in syms.values():
+            duped_names.add(name)
+            duped_syms.add((addr, name))
+            duped_key = None
+            for key, value in syms.items():
+                if value == name:
+                    duped_key = key
+                    break
+            assert duped_key is not None
+            del syms[duped_key]
+            continue
+        if addr in duped_addrs:
+            duped_syms.add((addr, name))
+            continue
+        if name in duped_names:
+            duped_syms.add((addr, name))
+            continue
+
+        syms[addr] = name
+    a.close()
+
+parse_flib_symbols(Path("asdf.txt"))
+
+b = FILEPATH.open()
 new_data = []
 for line in b:
     if ";" in line and " = " in line:
@@ -29,9 +70,16 @@ for line in b:
     new_data.append(line)
 b.close()
 
-b = open(FILEPATH, "w")
+b = FILEPATH.open("w")
 for line in new_data:
     b.write(line)
 b.close()
 
-print({f"0x{x:08X}" : syms[x] for x in set(syms.keys()) - found})
+print("symbols not found:")
+for key, value in {x: syms[x] for x in set(syms.keys()) - found}.items():
+    print("    ", f"0x{key:08X}", value)
+print()
+
+print("duplicated syms")
+for key, value in duped_syms:
+    print("    ", f"0x{key:08X}", value)
